32e5f1de6fbf0010d1e936be97db4ce219232ade
Arnab Nath <35195492+arnabnath90@users.noreply.github.com>
2023-09-26 14:17:00 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 12345
+const accountEmail = "arnabnath@gmail.com"
+const accountPassword = "1234567"
+accountCity = "Jaipur"
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+
+// Prefer not to use var because of block and functional scope
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+let num = 2^80
+console.log(typeof(num));
+// alert(3+3) this will not work as we are using node.js not browser
+
+
+//--------------primitive data types
+// number => 2 to the power 53
+// bigInt
+// string ""
+// boolean true/false
+// null standalone value
+// undefined variable is declared but not assigned/defined any value
+// symbol unique
+//-------------------
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
